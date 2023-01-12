e53a0678d3d9d21671940492e6a8474b84e6b382
Leagian <lea.giangrosso@gmail.com>
2023-01-12 16:05:03 +0100

    cards personnages

---
+++ src/components/Characters.js
@@ -0,0 +1,37 @@
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import CharactersCard from './CharactersCard'
+
+const Characters = () => {
+  const [charactersInfo, setCharactersInfos] = useState([])
+  const { regionId } = useParams()
+
+  useEffect(() => {
+    fetch('http://localhost:4242/')
+      .then(res => res.json())
+      .then(res => console.log(res) || setCharactersInfos(res))
+  }, [])
+
+  return (
+    <>
+      <div className='charac-global'>
+        {charactersInfo
+          .filter(
+            CharacterFilterRegion => CharacterFilterRegion.region === regionId
+          )
+
+          .map(CharacterMap => (
+            <CharactersCard
+              key={CharacterMap.id}
+              image={CharacterMap.image}
+              name={CharacterMap.name}
+              description={CharacterMap.description}
+            />
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default Characters
